f9a461a0912858b5c2466cd74995cbc5b3d2c6f9
Matt Lese <matthewjlese@gmail.com>
2026-09-01 13:11:10 -0700

    adds seeded gen test

--- test/names/core_test.clj
+++ test/names/core_test.clj
@@ -18,3 +18,12 @@
         (let [next-n (sut/next-n gen 15)]
           (is (= 15 (count next-n)))
           (is (every? #(and (string? %) (re-seq adj+noun-pattern %)) next-n)))))))
+
+(deftest seeded-generator-test
+  (testing "Can generate random words using a seed"
+    (let [gen-1 (sut/create {:seed 10})
+          gen-2 (sut/create {:seed 10})]
+      (is (= "thin-birthday" (sut/next gen-1) (sut/next gen-2)))
+      (is (= '("disillusioned-sound" "adhesive-stove" "picayune-mailbox" "rich-dinosaurs" "dreary-snow" "animated-cook")
+             (sut/next-n gen-1 6)
+             (sut/next-n gen-2 6))))))
